4707dfb84c2adc4e9a927882c336ee16cfc67b25
Aditya <adityatuteja7@gmail.com>
2023-10-04 17:06:22 +0530

    updated setting.py

--- OnlineFire_ReportingSystem/settings.py
+++ OnlineFire_ReportingSystem/settings.py
@@ -27,6 +27,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'firereport',
+    'whitenoise.runserver_nostatic'
 ]
 
 MIDDLEWARE = [
@@ -37,6 +38,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhitenoiseMiddleware'
 ]
 
 ROOT_URLCONF = 'OnlineFire_ReportingSystem.urls'
