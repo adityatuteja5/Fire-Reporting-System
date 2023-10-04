8b318697d209b20d40759053defa48811a03b6c5
Aditya <adityatuteja7@gmail.com>
2023-10-04 15:16:13 +0530

    updated setting.py

--- OnlineFire_ReportingSystem/settings.py
+++ OnlineFire_ReportingSystem/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = '5fv_1886ls8mb8fm^8mnniq=8z0)_jv(@4k6g7(czfmv82wxnz'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['fire-reporting.azurewebsites.net', '127.0.0.1']
 
 
 # Application definition
@@ -112,3 +112,8 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT=os.path.join(BASE_DIR,'media')
 
 DEFAULT_AUTO_FIELD='django.db.models.AutoField'
+
+
+#CSRF
+
+CSRF_TRUSTED_ORIGINS = ['https://fire-reporting.azurewebsites.net']
